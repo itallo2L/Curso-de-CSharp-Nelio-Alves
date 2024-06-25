375e1a877bf2afd7865ad7453fd490d938810c5d
itallo2L <itallocerqueiracosta@gmail.com>
2024-06-25 13:08:47 -0300

    Comecei a resolver o ProblemaDois

---
+++ ProblemaDois/Produto.cs
@@ -0,0 +1,25 @@
+﻿namespace ProblemaDois
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            var valorTotal = Quantidade * Preco;
+            return valorTotal;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+    }
+}
---
+++ ProblemaDois/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace ProblemaDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            var valorTotal = produto.ValorTotalEmEstoque();
+            Console.WriteLine($"Dados do produto: {produto.Nome}, $ {produto.Preco.ToString("F2", CultureInfo.InvariantCulture)}," +
+                $" {produto.Quantidade} unidades, Total: $ {valorTotal.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            //var quantidadeDeProduto = produto.
+        }
+    }
+}
